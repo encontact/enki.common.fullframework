35111e24f4e657adf68f1e6dd212b0b41ba682fe
Reinaldo <reinaldo.coelho@gmail.com>
2015-06-03 19:54:55 -0300

    Ajustes de configurações do appveyor para tentar publicar com Nuget

--- Enki.Common/Properties/AssemblyInfo.cs
+++ Enki.Common/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Enki.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("Enkilabs")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("pt-BR")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
