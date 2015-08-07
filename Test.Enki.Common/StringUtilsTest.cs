92c1fcbbce2e0448ac6b52fcaf18e7b4eeb7405f
Reinaldo <reinaldo.coelho@gmail.com>
2015-08-07 17:37:39 -0300

    Inclusão de testes automaticos para validação do remoção de acentos.

--- Test.Enki.Common/StringUtilsTest.cs
+++ Test.Enki.Common/StringUtilsTest.cs
@@ -48,5 +48,70 @@ namespace Test.Enki.Common {
 			Assert.AreEqual("1.402,95", StringUtils.format(StringUtils.CurrencyFrom("1,402.95").ToString(), "###.###.###,##"));
 			Assert.AreEqual("85,13", StringUtils.format(StringUtils.CurrencyFrom("R$ 85.13").ToString(), "###.###.###,##"));
 		}
+
+		[TestMethod()]
+		public void RemoveAccentsTest() {
+			Assert.AreEqual("A", StringUtils.RemoveAccents("Á"));
+			Assert.AreEqual("a", StringUtils.RemoveAccents("á"));
+			Assert.AreEqual("A", StringUtils.RemoveAccents("Â"));
+			Assert.AreEqual("a", StringUtils.RemoveAccents("â"));
+			Assert.AreEqual("A", StringUtils.RemoveAccents("Â"));
+			Assert.AreEqual("a", StringUtils.RemoveAccents("â"));
+			Assert.AreEqual("A", StringUtils.RemoveAccents("À"));
+			Assert.AreEqual("a", StringUtils.RemoveAccents("à"));
+			Assert.AreEqual("A", StringUtils.RemoveAccents("Å"));
+			Assert.AreEqual("a", StringUtils.RemoveAccents("å"));
+			Assert.AreEqual("A", StringUtils.RemoveAccents("Ã"));
+			Assert.AreEqual("a", StringUtils.RemoveAccents("ã"));
+			Assert.AreEqual("A", StringUtils.RemoveAccents("Ä"));
+			Assert.AreEqual("a", StringUtils.RemoveAccents("ä"));
+
+			Assert.AreEqual("E", StringUtils.RemoveAccents("É"));
+			Assert.AreEqual("e", StringUtils.RemoveAccents("é"));
+			Assert.AreEqual("E", StringUtils.RemoveAccents("Ê"));
+			Assert.AreEqual("e", StringUtils.RemoveAccents("ê"));
+			Assert.AreEqual("E", StringUtils.RemoveAccents("È"));
+			Assert.AreEqual("e", StringUtils.RemoveAccents("è"));
+			Assert.AreEqual("E", StringUtils.RemoveAccents("Ë"));
+			Assert.AreEqual("e", StringUtils.RemoveAccents("ë"));
+
+			Assert.AreEqual("I", StringUtils.RemoveAccents("Í"));
+			Assert.AreEqual("i", StringUtils.RemoveAccents("í"));
+			Assert.AreEqual("I", StringUtils.RemoveAccents("Î"));
+			Assert.AreEqual("i", StringUtils.RemoveAccents("î"));
+			Assert.AreEqual("I", StringUtils.RemoveAccents("Ì"));
+			Assert.AreEqual("i", StringUtils.RemoveAccents("ì"));
+			Assert.AreEqual("I", StringUtils.RemoveAccents("Ï"));
+			Assert.AreEqual("i", StringUtils.RemoveAccents("ï"));
+
+			Assert.AreEqual("O", StringUtils.RemoveAccents("Ó"));
+			Assert.AreEqual("o", StringUtils.RemoveAccents("ó"));
+			Assert.AreEqual("O", StringUtils.RemoveAccents("Ô"));
+			Assert.AreEqual("o", StringUtils.RemoveAccents("ô"));
+			Assert.AreEqual("O", StringUtils.RemoveAccents("Ò"));
+			Assert.AreEqual("o", StringUtils.RemoveAccents("ò"));
+			Assert.AreEqual("O", StringUtils.RemoveAccents("Õ"));
+			Assert.AreEqual("o", StringUtils.RemoveAccents("õ"));
+			Assert.AreEqual("O", StringUtils.RemoveAccents("Ö"));
+			Assert.AreEqual("o", StringUtils.RemoveAccents("ö"));
+
+			Assert.AreEqual("U", StringUtils.RemoveAccents("Ú"));
+			Assert.AreEqual("u", StringUtils.RemoveAccents("ú"));
+			Assert.AreEqual("U", StringUtils.RemoveAccents("Û"));
+			Assert.AreEqual("u", StringUtils.RemoveAccents("û"));
+			Assert.AreEqual("U", StringUtils.RemoveAccents("Ù"));
+			Assert.AreEqual("u", StringUtils.RemoveAccents("ù"));
+			Assert.AreEqual("U", StringUtils.RemoveAccents("Ü"));
+			Assert.AreEqual("u", StringUtils.RemoveAccents("ü"));
+
+			Assert.AreEqual("C", StringUtils.RemoveAccents("Ç"));
+			Assert.AreEqual("c", StringUtils.RemoveAccents("ç"));
+
+			Assert.AreEqual("N", StringUtils.RemoveAccents("Ñ"));
+			Assert.AreEqual("n", StringUtils.RemoveAccents("ñ"));
+
+			Assert.AreEqual("Y", StringUtils.RemoveAccents("Ý"));
+			Assert.AreEqual("y", StringUtils.RemoveAccents("ý"));
+		}
 	}
 }
