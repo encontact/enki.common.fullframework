1d4284e8bd2860565d9b6c2374efd38bf9aaacd3
reinaldocoelho <reinaldo.coelho@gmail.com>
2016-05-25 16:24:50 -0300

    Melhoria na lógica de validação de e-mails.
    Ajuste na lógica de validação de e-mails para aceitar mais um padrão.

--- Enki.Common/ValidateUtils.cs
+++ Enki.Common/ValidateUtils.cs
@@ -29,7 +29,7 @@ namespace Enki.Common
         public static bool ValidaEmail(string inputEmail)
         {
             inputEmail = inputEmail == null ? "" : inputEmail;
-            string strRegex = @"^[\w\+\=\-\.]+@[a-zA-Z0-9]{1}[a-zA-Z0-9\-]*?[a-zA-Z0-9]{1,}(\.{1}[a-zA-Z0-9]{2,})+?$|^[^<>]*?<\s*[\w\+\=\-\.]+@[a-zA-Z0-9]{1}[a-zA-Z0-9\-]*?[a-zA-Z0-9]{1,}(\.{1}[a-zA-Z0-9]{2,})+?\s*>$";
+            string strRegex = @"^[\w\+\=\-\.]+@[a-zA-Z0-9]{1}[a-zA-Z0-9\-]*?[a-zA-Z0-9]{1,}(\.{1}[a-zA-Z0-9\-]{2,})+?$|^[^<>]*?<\s*[\w\+\=\-\.]+@[a-zA-Z0-9]{1}[a-zA-Z0-9\-]*?[a-zA-Z0-9]{1,}(\.{1}[a-zA-Z0-9]{2,})+?\s*>$";
             var re = new Regex(strRegex, RegexOptions.IgnoreCase);
             return re.IsMatch(inputEmail);
         }
--- Test.Enki.Common/ValidateUtilsTests.cs
+++ Test.Enki.Common/ValidateUtilsTests.cs
@@ -27,6 +27,7 @@ namespace Enki.Common.Tests
             Assert.IsTrue(ValidateUtils.ValidaEmail("teste.email@meu-dominio.com.br"));
             Assert.IsTrue(ValidateUtils.ValidaEmail("teste.email@meu-dominio-dois.com.br"));
             Assert.IsTrue(ValidateUtils.ValidaEmail("Meu nome(meunome@teste.com) <meunome@teste.com>"));
+            Assert.IsTrue(ValidateUtils.ValidaEmail("teste.email@meu.dominio-internacional.com"));
 
             // Validações falsas.
             Assert.IsFalse(ValidateUtils.ValidaEmail("nomecomprido@"));
