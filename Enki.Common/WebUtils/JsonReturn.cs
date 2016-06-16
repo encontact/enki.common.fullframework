6df1d9512e408cc36602f2d543b85d89eaa9dbb4
reinaldocoelho <reinaldo.coelho@gmail.com>
2016-06-16 11:50:19 -0300

    Adição de classe para retorno de Json.

---
+++ Enki.Common/WebUtils/JsonReturn.cs
@@ -0,0 +1,16 @@
+﻿namespace Enki.Common.WebUtils
+{
+    public class JsonReturn
+    {
+        public object data { get; set; }
+        public bool status { get; set; }
+
+        public JsonReturn() { }
+
+        public JsonReturn(bool status, object data)
+        {
+            this.status = status;
+            this.data = data;
+        }
+    }
+}
